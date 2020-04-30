909f39669ebaecfed33b48c91f585e64fad3a4fa
Yag <yag@nothing.ch>
2020-04-30 14:00:22 +0200

    chore: passtrough emails

--- .eleventy.js
+++ .eleventy.js
@@ -18,6 +18,7 @@ module.exports = function (eleventyConfig) {
   });
 
   eleventyConfig.addPassthroughCopy({ "static/admin": "admin" });
+  eleventyConfig.addPassthroughCopy("mail");
   eleventyConfig.addPassthroughCopy("media");
 
   return {
